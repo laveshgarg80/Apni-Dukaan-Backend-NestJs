266aac7765a43fe9ff325da77c2fcd6065b60435
Lavesh Garg <lavesh.garg@Laveshs-MacBook-Pro.local>
2023-02-21 19:37:35 +0530

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { ProductsController } from './products/products.controller';
+
 import { ProductsModule } from './products/products.module';
-import { ProductsService } from './products/products.service';
+
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductsModule, AuthModule],
+  imports: [AuthModule, ProductsModule],
   controllers: [],
   providers: [],
   exports: [],
