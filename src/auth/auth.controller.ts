1bff5718ca5158ca6e3773496fc0f51a33707b21
Lavesh Garg <lavesh.garg@Laveshs-MacBook-Pro.local>
2023-02-21 20:01:04 +0530

    initial commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Header, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('/signin')
+  @Header('Access-Control-Allow-Origin', '*')
   getAllProducts(@Body() body): Promise<Object> {
     return this.authService.signIn(body);
   }
